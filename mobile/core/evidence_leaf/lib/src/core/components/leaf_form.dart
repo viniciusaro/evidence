d95f6bcf69f0472e9a00b333331ef5ceffe1ce3a
viniciusaro <vinicius.rodrigues@alice.com.br>
2023-09-15 15:30:42 -0300

    chore(leaf): remove unused class

--- mobile/core/evidence_leaf/lib/src/core/components/leaf_form.dart
+++
@@ -1,9 +0,0 @@
-import 'package:evidence_leaf/leaf.dart';
-
-class LeafForm extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
