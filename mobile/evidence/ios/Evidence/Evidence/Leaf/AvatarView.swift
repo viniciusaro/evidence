7a44e26cacf47b0feedede0864871c871a247b6f
Cris Messias <meunomeecriss@gmail.com>
2023-09-29 08:59:38 +0200

    :construction: working in avatar
    Creating a code that a optional asset photo

--- mobile/evidence/ios/Evidence/Evidence/Leaf/AvatarView.swift
+++ mobile/evidence/ios/Evidence/Evidence/Leaf/AvatarView.swift
@@ -9,25 +9,41 @@ import SwiftUI
 
 
 struct AvatarView: View {
+    var imageAvatar: String?
 
     var body: some View {
-        Image(systemName: "person.fill")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 100, height: 100)
-            .cornerRadius(100 / 2)
-            .overlay(
-                Circle()
-                    .stroke(Color.white, lineWidth: 4)
-                    .frame(width: 100, height: 100)
-            )
-            .shadow(radius: 10)
+                if let image = imageAvatar {
+                    Image(image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(80 / 2)
+                        .overlay {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 3)
+                                .frame(width: 80, height: 80)
+                        }
+                } else {
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 70, height: 70)
+                        .foregroundColor(.gray)
+                        .cornerRadius(80/2)
+                        .overlay {
+                            Circle()
+                                .stroke(Color.gray, lineWidth: 3)
+                                .frame(width: 80, height: 80)
+                        }
+                }
     }
+
 }
 
 
 struct AvatarView_Previews: PreviewProvider {
     static var previews: some View {
         AvatarView()
+        AvatarView(imageAvatar: "avatar")
     }
 }
