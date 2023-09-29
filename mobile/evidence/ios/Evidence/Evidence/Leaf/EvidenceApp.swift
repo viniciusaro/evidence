b907f9396332016d85183d93ad414de849f8d253
Cris Messias <meunomeecriss@gmail.com>
2023-09-29 07:54:12 +0200

    :construction: add icon for the app

---
+++ mobile/evidence/ios/Evidence/Evidence/Leaf/EvidenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvidenceApp.swift
+//  Evidence
+//
+//  Created by Vinicius Alves Rodrigues on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct EvidenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
