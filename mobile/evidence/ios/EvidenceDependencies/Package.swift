83dc2a705e99e7a8da76ae02c534f4dab33cee68
Vinicius Rodrigues <vinicius.rodrigues@alice.com.br>
2023-10-10 08:11:10 -0300

    chore(env): run tests github action (#9)

--- mobile/evidence/ios/EvidenceDependencies/Package.swift
+++ mobile/evidence/ios/EvidenceDependencies/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "EvidenceDependencies",
     platforms: [
-        .iOS(.v15), .macOS(.v10_15)
+        .iOS(.v15), .macOS(.v12)
     ],
     products: [
         .library(
