7d03bc99f54f350a4948b2a7d718e8eb857b4a39
Cris Messias <meunomeecriss@gmail.com>
2023-10-17 21:49:36 +0200

    Starting the LeafTag

---
+++ mobile/evidence/ios/Leaf/Sources/Leaf/Components/LeafTag.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Cris Messias on 15/10/23.
+//
+
+import SwiftUI
+
+struct LeafTag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeafTag()
+}
