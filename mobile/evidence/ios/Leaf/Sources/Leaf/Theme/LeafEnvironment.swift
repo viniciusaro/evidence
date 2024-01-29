14a608d554aff4d12b8c4c64f006c9628659764b
Cris Messias <meunomeecriss@gmail.com>
2024-01-29 13:17:21 +0100

    feat(leaf): add token to leaf/colors

--- mobile/evidence/ios/Leaf/Sources/Leaf/Theme/LeafEnvironment.swift
+++ mobile/evidence/ios/Leaf/Sources/Leaf/Theme/LeafEnvironment.swift
@@ -13,17 +13,27 @@ extension EnvironmentValues {
 
 public let defaultLeafTheme = LeafTheme(
     color: LeafTheme.Color(
-        font: LeafTheme.FontColors(
+        font: LeafTheme.TextColors(
             primary: Color(red: 29/255, green: 28/255, blue: 29/255),
             secondary: Color(red: 29/255, green: 28/255, blue: 29/255, opacity: 0.7),
+            tertiary: Color(red: 255/255, green: 255/255, blue: 255/255),
             link: Color(red: 18/255, green: 100/255, blue: 163/255)
         ),
-        system: LeafTheme.SystemColors(
+        button: LeafTheme.ButtonColors(
             buttonPrimary: Color(red: 0/255, green: 122/255, blue: 90/255),
-            avatarActive: Color(red: 46/255, green: 182/255, blue: 125/255),
+            buttonSecondary: Color(red: 255/255, green: 255/255, blue: 255/255),
+            buttonGoogle: Color(red: 72/255, green: 132/255, blue: 244/255)
+        ), 
+        warning: LeafTheme.WarningColors(
             success: Color(red: 18/255, green: 100/255, blue: 163/255),
             alert: Color(red: 236/255, green: 178/255, blue: 46/255),
-            error: Color(red: 224/255, green: 30/255, blue: 90/255)
+            error: Color(red: 224/255, green: 30/255, blue: 90/255),
+            errorBG: Color(red: 224/255, green: 30/255, blue: 90/255, opacity: 0.1)
+        ),
+        system: LeafTheme.SystemColors(
+            aubergineBG: Color(red: 74/255, green: 21/255, blue: 75/255),
+            avatarActive: Color(red: 29/255, green: 28/255, blue: 29/255, opacity: 0.4),
+            avatarAway: Color(red: 46/255, green: 182/255, blue: 125/255)
         ),
         custom: LeafTheme.CustomThemeColors(
             topBar: Color(red: 74/255, green: 21/255, blue: 75/255),
@@ -37,17 +47,27 @@ public let defaultLeafTheme = LeafTheme(
 
 public let darkLeafTheme = LeafTheme(
     color: LeafTheme.Color(
-        font: LeafTheme.FontColors(
+        font: LeafTheme.TextColors(
             primary: Color(red: 255/255, green: 255/255, blue: 255/255),
             secondary: Color(red: 244/255, green: 237/255, blue: 228/255),
+            tertiary: Color(red: 255/255, green: 255/255, blue: 255/255),
             link: Color(red: 18/255, green: 100/255, blue: 163/255)
         ),
-        system: LeafTheme.SystemColors(
+        button: LeafTheme.ButtonColors(
             buttonPrimary: Color(red: 0/255, green: 122/255, blue: 90/255),
-            avatarActive: Color(red: 46/255, green: 182/255, blue: 125/255),
+            buttonSecondary: Color(red: 255/255, green: 255/255, blue: 255/255),
+            buttonGoogle: Color(red: 72/255, green: 132/255, blue: 244/255)
+        ),
+        warning: LeafTheme.WarningColors(
             success: Color(red: 18/255, green: 100/255, blue: 163/255),
             alert: Color(red: 236/255, green: 178/255, blue: 46/255),
-            error: Color(red: 224/255, green: 30/255, blue: 90/255)
+            error: Color(red: 224/255, green: 30/255, blue: 90/255),
+            errorBG: Color(red: 224/255, green: 30/255, blue: 90/255, opacity: 0.1)
+        ),
+        system: LeafTheme.SystemColors(
+            aubergineBG: Color(red: 74/255, green: 21/255, blue: 75/255),
+            avatarActive: Color(red: 244/255, green: 237/255, blue: 228/255, opacity: 0.5),
+            avatarAway: Color(red: 46/255, green: 182/255, blue: 125/255)
         ),
         custom: LeafTheme.CustomThemeColors(
             topBar: Color(red: 74/255, green: 21/255, blue: 75/255),
--- mobile/evidence/ios/Leaf/Sources/Leaf/Theme/LeafTheme.swift
+++ mobile/evidence/ios/Leaf/Sources/Leaf/Theme/LeafTheme.swift
@@ -7,23 +7,37 @@ public struct LeafTheme {
 
 extension LeafTheme {
     public struct Color {
-        public let font: FontColors
+        public let font: TextColors
+        public let button: ButtonColors
+        public let warning: WarningColors
         public let system: SystemColors
         public let custom: CustomThemeColors
     }
 
-    public struct FontColors {
+    public struct TextColors {
         public let primary: SwiftUI.Color
         public let secondary: SwiftUI.Color
+        public let tertiary: SwiftUI.Color
         public let link: SwiftUI.Color
     }
 
-    public struct SystemColors {
+    public struct ButtonColors {
         public let buttonPrimary: SwiftUI.Color
-        public let avatarActive: SwiftUI.Color
+        public let buttonSecondary: SwiftUI.Color
+        public let buttonGoogle: SwiftUI.Color
+    }
+
+    public struct WarningColors {
         public let success: SwiftUI.Color
         public let alert: SwiftUI.Color
         public let error: SwiftUI.Color
+        public let errorBG: SwiftUI.Color
+    }
+
+    public struct SystemColors {
+        public let aubergineBG: SwiftUI.Color
+        public let avatarActive: SwiftUI.Color
+        public let avatarAway: SwiftUI.Color
     }
 
     public struct CustomThemeColors {
