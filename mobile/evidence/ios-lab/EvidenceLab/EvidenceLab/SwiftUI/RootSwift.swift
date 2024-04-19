7417fde3ad13c40e5b9a85116386eef7f35753a6
Vinicius Rodrigues <vinicius.a.ro@gmail.com>
2024-04-19 16:11:06 -0300

    working chat with bindings

--- mobile/evidence/ios-lab/EvidenceLab/EvidenceLab/SwiftUI/RootSwift.swift
+++ mobile/evidence/ios-lab/EvidenceLab/EvidenceLab/SwiftUI/RootSwift.swift
@@ -2,239 +2,92 @@ import Combine
 import IdentifiedCollections
 import SwiftUI
 
-#Preview("Root") {
+#Preview("ChatList") {
     authClient = AuthClient.authenticated()
     dataClient = DataClient.mock(Chat.mockList)
-    stockClient = StockClient.live
+    stockClient = StockClient.mock(Chat.mockList, interval: 10000)
     installationClient = InstallationClient.mock("1")
 
-    return RootViewSwift(
-        model: RootViewModel()
-    )
+    return ChatApp()
 }
 
-#Preview("Home") {
-    authClient = AuthClient.authenticated()
-    dataClient = DataClient.mock(Chat.mockList)
-    stockClient = StockClient.live
-    installationClient = InstallationClient.mock("1")
-
-    return HomeViewSwift(
-        model: HomeViewModel(
-            state: .init(selectedTab: .chatList)
-        )
-    )
-}
-
-enum RootViewState {
-    case home(HomeViewModel)
-    case login
-}
-
-@Observable
-class RootViewModel {
-    var state: RootViewState
-    
-    init() {
-        if let _ = authClient.getAuthenticatedUser() {
-            self.state = .home(HomeViewModel())
-        } else {
-            self.state = .login
-        }
-    }
-}
-
-struct RootViewSwift: View {
-    let model: RootViewModel
+struct ChatApp: View {
+    @Bindable var store = ChatStore()
     
     var body: some View {
-        switch model.state {
-        case let .home(homeModel):
-            HomeViewSwift(model: homeModel)
-        case .login:
-            LoginViewSwift()
-        }
-    }
-}
-
-struct LoginViewSwift: View {
-    var body: some View {
-        Text("login")
-    }
-}
-
-struct HomeViewState {
-    var chatList: ChatListViewModel = .init()
-    var selectedTab: Tab = .chatList
-    
-    enum Tab: String, Codable {
-        case chatList = "Conversas"
-        case profile = "Perfil"
-        
-        var title: String {
-            return rawValue.capitalized
-        }
+        ChatListViewSwift(chats: $store.chats)
     }
 }
 
 @Observable
-class HomeViewModel {
-    var state: HomeViewState
-    
-    init(state: HomeViewState = .init()) {
-        self.state = state
-    }
-}
-
-struct HomeViewSwift: View {
-    @Bindable var model: HomeViewModel
-    
-    var body: some View {
-        TabView(selection: $model.state.selectedTab) {
-            ChatListViewSwift(model: model.state.chatList)
-                .tabItem {
-                    Label(
-                        HomeViewState.Tab.chatList.title,
-                        systemImage: "bubble.right"
-                    )
-                }
-                .tag(HomeViewState.Tab.chatList)
-            ProfileViewSwift()
-                .tabItem {
-                    Label(
-                        HomeViewState.Tab.profile.title,
-                        systemImage: "brain.filled.head.profile"
-                    )
-                }
-                .tag(HomeViewState.Tab.profile)
-        }
-    }
-}
-
-struct ChatListState {
-    var chats: IdentifiedArrayOf<ChatClass> = [] {
-        didSet {
-            print("update")
-        }
-    }
-    var detail: ChatDetailModel? = nil
+class ChatStore {
+    var chats: [Chat] = []
+    private var cancellable: AnyCancellable?
     
     init() {
         do {
             let data = try dataClient.load(.chats)
             let decoder = JSONDecoder()
-            let chats = try decoder.decode(IdentifiedArrayOf<Chat>.self, from: data)
-            self.chats = chats.map { ChatClass(from: $0) }.identified
+            self.chats = try decoder.decode([Chat].self, from: data)
         } catch {
             self.chats = []
         }
-    }
-}
-
-@Observable
-class ChatListViewModel {
-    var state: ChatListState {
-        didSet {
-            print("update state")
-        }
-    }
-    private var cancellable: AnyCancellable?
-    
-    init(state: ChatListState = .init()) {
-        self.state = state
+        
         self.consumePendingMessages()
     }
     
     private func consumePendingMessages() {
         self.cancellable = stockClient.consume()
             .sink(receiveValue: { chatUpdate in
-                if let chat = self.state.chats[id: chatUpdate.id] {
-                    self.state.chats[id: chat.id]?
-                        .messages.append(contentsOf: chatUpdate.messages)
+                if let index = self.chats.firstIndex(where: { $0.id == chatUpdate.id }) {
+                    self.chats[index].messages.append(contentsOf: chatUpdate.messages)
                 } else {
-                    self.state.chats.insert(ChatClass(from: chatUpdate), at: 0)
+                    self.chats.insert(chatUpdate, at: 0)
                 }
             })
-    }
-    
-    func onListItemTapped(_ chat: ChatClass) {
-        state.detail = ChatDetailModel(chat: chat)
     }
 }
 
 struct ChatListViewSwift: View {
-    @Bindable var model: ChatListViewModel
+    @Binding var chats: [Chat]
+    @State private var detail: Chat.ID? = nil
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(model.state.chats) { chat in
-                    Button(action: {
-                        model.onListItemTapped(chat)
-                    }, label: {
+                ForEach($chats) { $chat in
+                    Button {
+                        self.detail = chat.id
+                    } label: {
                         VStack(alignment: .leading) {
                             Text(chat.name)
                             if let content = chat.messages.last?.content {
                                 Text(content).font(.caption)
                             }
                         }
-                    })
-                }
-                .onDelete { indexSet in
-    //                store.send(.onListItemDelete(indexSet))
+                    }
                 }
             }
-            .animation(.bouncy, value: model.state.chats)
+            .animation(.bouncy, value: chats)
             .listStyle(.plain)
-            .navigationDestination(item: $model.state.detail) { model in
-                ChatDetailViewSwift(model: model)
+            .navigationDestination(item: $detail) { id in
+                let index = chats.firstIndex(where: { $0.id == id })!
+                return ChatDetailViewSwift(chat: $chats[index])
             }
         }
     }
 }
 
-struct ChatDetailState: Equatable, Hashable {
-    var chat: ChatClass
-    var inputText: String = ""
-}
-
-@Observable
-class ChatDetailModel {
-    var state: ChatDetailState
-    
-    init(chat: ChatClass) {
-        self.state = .init(chat: chat)
-    }
-    
-    func onSendButtonTapped() {
-        let newMessage = Message(content: state.inputText, sender: .cris)
-        state.chat.messages.append(newMessage)
-        state.inputText = ""
-    }
-}
-
-extension ChatDetailModel: Equatable, Hashable {
-    static func == (lhs: ChatDetailModel, rhs: ChatDetailModel) -> Bool {
-        lhs.state == rhs.state
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(state)
-    }
-}
-
 struct ChatDetailViewSwift: View {
-    @Bindable var model: ChatDetailModel
-    
+    @Binding var chat: Chat
+    @State var inputText: String = ""
+
     var body: some View {
         VStack {
             List {
-                ForEach(model.state.chat.messages) { message in
+                ForEach(chat.messages) { message in
                     Text(message.content)
                 }
-//                ForEach(store.scope(state: \.messages, action: \.message)) { store in
-//                    MessageView(store: store)
-//                }
             }
             HStack(spacing: 12) {
                 Button(action: {
@@ -245,15 +98,15 @@ struct ChatDetailViewSwift: View {
                         .font(.title)
                         .foregroundStyle(.primary)
                 })
-                TextField("", text: $model.state.inputText)
+                TextField("", text: $inputText)
                 .frame(height: 36)
                 .padding([.leading, .trailing], 8)
                 .background(RoundedRectangle(cornerRadius: 18)
                     .fill(Color(red: 43/255, green: 43/255, blue: 43/255))
                 )
                 Button(action: {
-//                    store.send(.send)
-                    model.onSendButtonTapped()
+                    chat.messages.append(Message(content: inputText, sender: .vini))
+                    inputText = ""
                 }, label: {
                     Label("", systemImage: "arrow.forward.circle.fill")
                         .labelStyle(.iconOnly)
@@ -265,14 +118,6 @@ struct ChatDetailViewSwift: View {
             .background(Color(red: 20/255, green: 20/255, blue: 20/255))
         }
         .listStyle(.plain)
-//        .navigationTitle(store.title)
-    }
-}
-
-struct ProfileViewSwift: View {
-//    @Environment(UserObject.self) var user
-    
-    var body: some View {
-        Text("Profile")
+        .navigationTitle(chat.name)
     }
 }
