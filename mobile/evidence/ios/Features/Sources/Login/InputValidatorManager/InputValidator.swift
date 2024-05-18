1b9b1910ec5eceb7851675f0d96f6e88a180f512
Cris Messias <meunomeecriss@gmail.com>
2024-05-18 14:01:47 +0200

    add more test case to inputValidator

--- mobile/evidence/ios/Features/Sources/Login/InputValidatorManager/InputValidator.swift
+++ mobile/evidence/ios/Features/Sources/Login/InputValidatorManager/InputValidator.swift
@@ -10,7 +10,7 @@ import Foundation
 
 final public class InputValidator {
     func isValidEmail(_ email: String) -> Bool {
-        let regex = try! NSRegularExpression(pattern: "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}$", options: [.caseInsensitive])
+        let regex = try! NSRegularExpression(pattern: "^[A-Z0-9._%+-]+@[A-Z0-9]+(\\.[A-Z0-9]+)?\\.[A-Z]{2,6}$" , options: [.caseInsensitive])
         return regex.firstMatch(in: email, options: [], range: NSRange(location: 0, length: email.utf16.count)) != nil
     }
 
--- mobile/evidence/ios/Features/Tests/LoginTests/InputValidatorTests.swift
+++ mobile/evidence/ios/Features/Tests/LoginTests/InputValidatorTests.swift
@@ -14,11 +14,26 @@ final class InputValidatorTests: XCTestCase {
     @Dependency(\.inputValidator) var inputValidator
 
     func testIsValidEmailValid() {
-        XCTAssertTrue(inputValidator.isValidEmail("test@example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user@example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user91@example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user@sub.example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user.test@sub.example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user.test@example.com"))
+        XCTAssertTrue(inputValidator.isValidEmail("user-test@example.com"))
     }
 
     func testIsValidEmailInvalid() {
-        XCTAssertFalse(inputValidator.isValidEmail("invalid-email"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@test"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@test-a.com"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@-test.com"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@.com"))
+        XCTAssertFalse(inputValidator.isValidEmail("@user"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@email.com.."))
+        XCTAssertFalse(inputValidator.isValidEmail("user@email..com."))
+        XCTAssertFalse(inputValidator.isValidEmail("user@email-a.com"))
+        XCTAssertFalse(inputValidator.isValidEmail("user@email.com.br.io"))
     }
 
     func testIsValidEmailEmpty() {
@@ -26,14 +41,14 @@ final class InputValidatorTests: XCTestCase {
     }
 
     func testIsValidPasswordValid() {
-        XCTAssertTrue(inputValidator.isValidEmail("test@example.com"))
+        XCTAssertTrue(inputValidator.isValidPassword("moreThen8Caracteres"))
     }
 
     func testIsValidPasswordInvalid() {
-        XCTAssertFalse(inputValidator.isValidEmail("invalid-email"))
+        XCTAssertFalse(inputValidator.isValidPassword("123"))
     }
 
     func testIsValidPasswordEmpty() {
-        XCTAssertFalse(inputValidator.isValidEmail(""))
+        XCTAssertFalse(inputValidator.isValidPassword(""))
     }
 }
