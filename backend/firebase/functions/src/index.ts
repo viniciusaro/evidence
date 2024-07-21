0de92e85e3a1933c6fcda60ee07d985b76116452
Vinicius Rodrigues <viniciusrodrigues@Viniciuss-MacBook-Pro.local>
2024-07-21 14:15:47 -0300

    feat: using cloud functions to call gemini api

--- backend/firebase/functions/src/index.ts
+++ backend/firebase/functions/src/index.ts
@@ -8,12 +8,28 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
+import {GoogleGenerativeAI} from "@google/generative-ai";
+import {defineSecret} from "firebase-functions/params";
+import {onInit} from "firebase-functions/v2/core";
 import * as logger from "firebase-functions/logger";
+
+const apiKey = defineSecret("GEMINI_API_KEY");
+let genAI: GoogleGenerativeAI;
+
+onInit(() => {
+  genAI = new GoogleGenerativeAI(apiKey.value());
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const geminiBot = onRequest(async (request, response) => {
+  logger.info("on request");
+  const message = request.body["message"];
+  logger.info("message received: " + message);
+
+  const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
+  const result = await model.generateContent(message);
+  const responseText = result.response.text();
+  response.send(responseText);
+});
